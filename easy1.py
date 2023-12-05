21b39fd1646318e71e30eb8fbaaa04bfe7b5dd6f
RanjanaShetty <92917118+RanjanaShetty@users.noreply.github.com>
2023-12-05 11:54:10 +0530

    easy1.py

--- easy1.py
+++ easy1.py
@@ -1,10 +1,10 @@
-st=input()
+s=input()
 count=0
-for i in range(-1,-len(st)-1,-1):
-    if st[i]==' ' and count==0:
+for i in range(-1,-len(s)-1,-1):
+    if s[i]==' ' and count==0:
         continue
-    if st[i]!=' ':
+    if s[i]!=' ':
         count+=1
-    elif st[i]==' ':
+    elif s[i]==' ':
         break
 print(count)
